f2183b5582d4b9f296486cf1e15bc01eaa70c04b
pkiop <pkiopb@gmail.com>
2022-10-26 07:49:14 +0900

    v0

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 mod api;
-// mod model;
-// mod repository;
+mod model;
+mod repository;
 
 use api::task::{
     get_task,
-    // submit_task,
-    // start_task,
-    // complete_task,
-    // pause_task,
-    // fail_task,
+    submit_task,
+    start_task,
+    complete_task,
+    pause_task,
+    fail_task,
 };
-// use repository::ddb::DDBRepository;
+use repository::ddb::DDBRepository;
 use actix_web::{HttpServer, App, web::Data, middleware::Logger};
 
 #[actix_web::main]
@@ -21,23 +21,26 @@ async fn main() -> std::io::Result<()> {
 
     let config = aws_config::load_from_env().await;
     HttpServer::new(move || {
-        // let ddb_repo: DDBRepository = DDBRepository::init(
-        //     String::from("task"),
-        //     config.clone()
-        // );
-        // let ddb_data = Data::new(
-        //     ddb_repo
-        // );
+        // 매 thread 마다 새로 쓰도록
+        let ddb_repo: DDBRepository = DDBRepository::init(
+            String::from("task"), // table name
+            config.clone()
+        );
+
+        let ddb_data = Data::new(
+            ddb_repo
+        );
+
         let logger = Logger::default();
         App::new()
             .wrap(logger)
-            // .app_data(ddb_data)
+            .app_data(ddb_data)
             .service(get_task)
-            // .service(submit_task)
-            // .service(start_task)
-            // .service(complete_task)
-            // .service(pause_task)
-            // .service(fail_task)
+            .service(submit_task)
+            .service(start_task)
+            .service(complete_task)
+            .service(pause_task)
+            .service(fail_task)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
